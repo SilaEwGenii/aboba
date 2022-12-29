86b0a8afcb14f0932925e8951f64feaa9f790184
Evgenii <silaewgenii@gmail.com>
2022-12-29 18:49:18 +0300

    norm add devise, no flash

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-    # before_action :authenticate_user!, exept: [:index, :show]
+    before_action :authenticate_user!, exept: [:index, :show]
 
 
     def index
