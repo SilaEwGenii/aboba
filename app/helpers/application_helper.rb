82013e6425792805b9da04b3bb441dca8622e383
Evgenii <silaewgenii@gmail.com>
2022-12-29 19:10:56 +0300

    norm add devise, no flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,4 @@
 module ApplicationHelper
 
-    def bootstrap_class_for(name)
-        {
-            success: "allert-success",
-            error: "allert-danger",
-            danger: "allert-danger",
-            alert: "allert-warning",
-            notice: "allert-info"
-        }[name.to_sym] || name
-    end
-
 
 end
